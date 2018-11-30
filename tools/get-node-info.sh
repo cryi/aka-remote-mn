f80c37155d1f8f9a22d3b07ccf4420b14abee077
cryi <cryi@tutanota.com>
2018-11-30 14:45:25 +0100

    improvements & fixes

--- tools/get-node-info.sh
+++ tools/get-node-info.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 container=$(docker-compose -f $BASEDIR/../docker-compose.yml ps -q mn)
-docker exec -it $container /home/akroma/get-node-info.sh
+docker exec $container /home/akroma/get-node-info.sh
