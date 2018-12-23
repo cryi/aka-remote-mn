e489b43b898405d427f7c230e4e407a252a86ec3
cryi <cryi@tutanota.com>
2018-12-23 23:50:54 +0100

    Updated tools scripts

--- tools/allow-container-write.sh
+++ tools/allow-container-write.sh
@@ -1,56 +1,61 @@
-#!/bin/bash
+#!/bin/sh
 
-function script_usage() {
+script_usage() {
     cat << EOF
+Allows write for user id 1000 (or remap uid +1000) inside containers
+
 Usage:
      -h|--help                  Displays this help
-     -v|--verbose               Displays verbose output
-    -nc|--no-colour             Disables colour output
-    -cr|--cron                  Run silently unless we encounter an error
+     -p=[path]|--path=[path]    Sets rights for specified file/directory
+     -f|--force                 Wont ask for confirmation
 EOF
 }
 
-function parse_params() {
-    local param
-    while [[ $# -gt 0 ]]; do
-        param="$1"
-        shift
-        case $param in
+parse_params() {
+    while :; do
+        case $1 in
             -h|--help)
                 script_usage
                 exit 0
                 ;;
             -f|--force)
                 force=true
-                ;;
-            -p|--path)
-                path="$1"
                 shift
                 ;;
-            *)
-                echo "Invalid parameter was provided: $param"
+            -p=*|--path=*)
+                path=$(echo $1 | sed 's/-p=//g')
+                path=$(echo $path | sed 's/--path=//g')
+                echo $path
+                shift
+                ;;
+            -?*)
+                echo "Invalid parameter was provided: $1"
                 exit 2
                 ;;
+            *)
+                break;
         esac
     done
 }
+
 parse_params "$@"
 
 if [ ! -d "$path" ] && [ ! -f "$path" ]; then
-        echo "Path '$1' not found!";
+        echo "Path '$path' not found!";
         exit;
 fi;
 uid=$(grep "dockremap" /etc/subuid)
 uid=$(echo $uid | cut -d ":" -f 2)
 uid=$(($uid + 1000))
-if [ $force = true ]; then
+
+if [ "$force" = "true" ]; then
     REPLY="y"
 else
-    read -p "Do you really make '$path' writable from containers (!!! do not use for system folders !!!)? (y/N) " -n 1 -r
+    echo "Do you really make '$path' writable from containers (!!! do not use for system folders !!!)? (y/N) "
+    read REPLY
     echo
 fi
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ]; then
     chown -R $uid:$uid $path &&  echo "Access permission to '$path' set to uid $uid (allows container to write into fs)"
     exit $?
 fi
--- tools/get-node-info.sh
+++ tools/get-node-info.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 BASEDIR=$(dirname "$0")
 container=$(docker-compose -f $BASEDIR/../docker-compose.yml ps -q mn)
 docker exec $container /home/akroma/get-node-info.sh
