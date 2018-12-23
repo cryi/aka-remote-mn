52378c58fca451bda77dd20e95450fb83df9cbe8
cryi <cryi@tutanota.com>
2018-12-24 00:42:00 +0100

    added proper license notice

--- containers/aka/get-version.sh
+++ containers/aka/get-version.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
+
+# AKROMA REMOTE MASTERNODE SETUP AND MANAGEMENT
+# Copyright (C) 2018 cryon.io
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+# Contact: cryi@tutanota.com
+
 ver=$(/usr/sbin/geth-akroma version | grep "Version: " | grep "akroma" | sed "s/Version: //" |  sed "s/-akroma//")
 printf "$ver"
--- containers/nginx/gen-temp-certs.sh
+++ containers/nginx/gen-temp-certs.sh
@@ -1,3 +1,20 @@
+# AKROMA REMOTE MASTERNODE SETUP AND MANAGEMENT
+# Copyright (C) 2018 cryon.io
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+# Contact: cryi@tutanota.com
 
 if [ ! -f "/certs/privkey.pem" ] || [ ! -f "/certs/fullchain.pem" ]; then
     openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 \
